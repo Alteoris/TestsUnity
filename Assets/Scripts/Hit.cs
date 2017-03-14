bbfc5b3863e3afd536f45262f282b32c1609f04a
Alexandre Bonnet <alexandre.bonnet@econocom.com>
2017-03-14 14:18:25 +0100

    Udate scene

---
+++ Assets/Scripts/Hit.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Hit : MonoBehaviour
+{
+    public Transform NextTarget;
+
+    void OnTriggerEnter(Collider other)
+    {
+        MoveTo.Instance.ChangeTarget(NextTarget);
+    }
+}
--- Assets/Scripts/MoveTo.cs
+++ Assets/Scripts/MoveTo.cs
@@ -3,12 +3,21 @@ using UnityEngine.AI;
 
 public class MoveTo : MonoBehaviour
 {
+    private NavMeshAgent agent;
 
     public Transform goal;
+    public static MoveTo Instance { get; private set; }
 
     void Start()
     {
-        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        Instance = this;
+
+        agent = GetComponent<NavMeshAgent>();
         agent.destination = goal.position;
+    }
+
+    public void ChangeTarget(Transform newTarget)
+    {
+        agent.destination = newTarget.position;
     }
 }
